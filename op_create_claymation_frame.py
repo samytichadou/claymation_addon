ff1e315c0ceafa9b6db726623f8751b6683eee8d
samytichadou <samytichadou@gmail.com>
2020-10-23 12:21:32 +0200

    poll improvement and switch on frame creation

--- op_create_claymation_frame.py
+++ op_create_claymation_frame.py
@@ -12,7 +12,12 @@ class CLAYMATION_OT_create_frame(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        return context.object.type in {"MESH", "CURVE"}
+        if context.object.type in {"MESH", "CURVE"}:
+            try:
+                context.object.data.shape_keys.key_blocks[claymation_prefix + str(context.scene.frame_current)]
+                return False
+            except (AttributeError, KeyError):
+                return True
 
 
     def execute(self, context):
@@ -38,6 +43,9 @@ class CLAYMATION_OT_create_frame(bpy.types.Operator):
         except (AttributeError, KeyError):
             new_sk = obj.shape_key_add(name=sk_name)
             new_sk.value = 1
-            if scn.claymation_debug: print(claymation_print + "Claymation Frame Created") #debug
+            
+        obj.active_shape_key_index = len(obj.data.shape_keys.key_blocks)-1
+
+        if scn.claymation_debug: print(claymation_print + "Claymation Frame Created") #debug
 
         return {'FINISHED'}
